42652de9ac758ee90830decc5e18524fcd167a62
deptula_krzysztof <krzysztof.deptula@apagroup.pl>
2022-01-15 23:51:55 +0100

    login page and user menu

--- src/app/components/atoms/Avatar/Avatar.style.js
+++ src/app/components/atoms/Avatar/Avatar.style.js
@@ -1,5 +1,17 @@
-import styled from 'styled-components';
+import styled,{ keyframes } from 'styled-components';
 import { Theme } from 'assets/styles/Theme';
+
+const showDrop = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(0);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(62px);
+  }
+`;
 
 export const StyledAvatar = styled.div`
   display: flex;
@@ -8,6 +20,7 @@ export const StyledAvatar = styled.div`
 `;
 
 export const UserFace = styled.div`
+  z-index: 100;
   display: inline-block;
   width: 48px;
   height: 48px;
@@ -21,18 +34,20 @@ export const UserFace = styled.div`
 `;
 
 export const UserMenu = styled.ul`
-  z-index: 100;
+  z-index: 99;
   position: absolute;
+  transform: translateY(62px);
+  overflow: hidden;
   inset: 0px 0px auto auto;
   margin: 0px;
-  transform: translate(0, 62px);
   flex-direction: column;
   border-radius: 4px;
   background-color: white;
   box-shadow: 0px 8px 32px rgba(17, 18, 20, 0.16);
-  padding: 4px 0;
+  padding: 0;
   list-style: none;
   width: 184px;
+  animation: ${showDrop} .25s ease 1;
 `;
 
 export const ListItem = styled.li`
@@ -42,9 +57,9 @@ export const ListItem = styled.li`
   width: 100%;
   cursor: pointer;
   user-select: none;
+  transition: color 0.125s linear forwards;
 
   &:hover {
     color: ${Theme.colors.primary};
-    background: ${Theme.colors.grey20};
   }
 `;
