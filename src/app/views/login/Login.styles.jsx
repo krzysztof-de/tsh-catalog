b1cb7fd013ef36ac3d1180ef504d9b3d7ec1a010
deptula_krzysztof <krzysztof.deptula@apagroup.pl>
2022-01-16 02:09:00 +0100

    login page lauout

--- src/app/views/login/Login.styles.jsx
+++ src/app/views/login/Login.styles.jsx
@@ -10,7 +10,6 @@ export const Wrapper = styled.div`
 
 export const StyledForm = styled.div`
   max-width: 500px;
-
   button[type='submit'] {
     margin: 56px 0 16px 0;
     height: 48px;
@@ -19,34 +18,30 @@ export const StyledForm = styled.div`
 `;
 
 export const Photo = styled.div`
-  width: 40%;
+  flex: 3;
   background-image: url(/images/bridge.jpg);
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
-  transition: width 0.25s ease-in-out;
+  transition: flex 0.25s ease-in-out;
 
   @media (max-width: 660px) {
-    width: 0%;
+    flex: 0;
   }
 `;
 
 export const RightSide = styled.div`
   background: white;
+  flex: 4;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   padding: 50px 14vw 50px 8vw;
-  width: 60%;
-  transition: width 0.25s ease-in-out;
+  transition: flex 0.25s ease-in-out;
 
   @media (max-width: 660px) {
     padding: 50px 8vw;
-    width: 100%;
+    flex: 1;
   }
 `;
-export const FutureFooter = styled.div`
-
-`;
-
-
+export const FutureFooter = styled.div``;
